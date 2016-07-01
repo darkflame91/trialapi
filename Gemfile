5bef7fdb9e299cbd38155f449b5e134c86925532
Dominic <darkflame91@gmail.com>
2016-07-01 21:55:19 +0530

    Minor

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
 
+
 # Nath told me to install this.
 gem 'paperclip'
 # gem 'activeadmin'
--- db/migrate/20160701151749_create_case_media.rb
+++ db/migrate/20160701151749_create_case_media.rb
@@ -2,7 +2,7 @@ class CreateCaseMedia < ActiveRecord::Migration
   def change
     create_table :case_media do |t|
       t.references :case, index: true, foreign_key: true
-      t.references :mediatype, index: true, foreign_key: true
+      t.references :media_type, index: true, foreign_key: true
       t.string :mediacode
       t.has_attached_file :media
 
